10ec4911b368e2c16b4c3ca31763e8d7a7369c08
Mohammed Mahmood <mohammed.hamidf99@gmail.com>
2021-06-24 16:41:26 +0000

    Automatically backed up by Learn

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -15,7 +15,11 @@ class Filters extends React.Component {
         </div>
 
         <div className="field">
-          <button className="ui secondary button">Find pets</button>
+          <button
+            onClick={this.props.onFindPetsClick}
+            className="ui secondary button">
+            Find pets
+          </button>
         </div>
       </div>
     )
